4da482fb20b104e6dd37616c0919d2295876ee63
Ricardo Cuevas <jrcuevas@unbosque.edu.co>
2021-03-09 19:02:38 -0500

    Add Pet

--- js/load-csv.js
+++ js/load-csv.js
@@ -3,8 +3,9 @@ var alData;
 d3.dsv( ";", "../Data/pets-citizens.csv" )
   .then( function( data ) {
   	alData = data; 
-  	printData();   
+  	printData(); 
   } )
+
   .catch( function( error ) {
     // handle error   
   } );
@@ -12,3 +13,26 @@ d3.dsv( ";", "../Data/pets-citizens.csv" )
   function printData(){
   	console.log(alData);
   }
+
+  function prueba(){
+
+    console.log("robin hijo de la verga");
+    var microchip = document.getElementById('Microchip').value; 
+    var species = document.getElementById('cf').value; 
+    var species = document.getElementById('mh').value; 
+    console.log(escribir); 
+
+    allData.push({"microchip" : "1312312", "species" : "1312312", "sex" : "1312312", "size" : "1312312", "potentDangerous" : "1312312", "neighborhood" : "1312312"});
+    printData(); 
+
+    
+ }
+
+ function manojoDeArreglo(){
+
+  console.log(allData[allData.length-1]);
+  allData.push({"microchip" : "1312312", "species" : "1312312", "sex" : "1312312", "size" : "1312312", "potentDangerous" : "1312312", "neighborhood" : "1312312"});
+  console.log(allData[allData.length-1]);
+
+
+}
