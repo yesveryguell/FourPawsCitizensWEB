215a4e064fafffaa2fdb8d9297febe1611a58f00
Felipe Rojas <jfrojasr@unbosque.edu.co>
2021-03-11 16:33:28 -0500

    updated some errors, form is correct

--- js/load-csv.js
+++ js/load-csv.js
@@ -17,24 +17,46 @@ d3.dsv( ";", "../Data/pets-citizens.csv" )
 
   function prueba(){
 
-    console.log("robin hijo de la verga");
-    var microchip = document.getElementById('Microchip').value; 
-    var species = document.getElementById('c').value; 
-    var species2 = document.getElementById('f').value; 
-    var sex = document.getElementById('m').value;
-    var sex2 = document.getElementById('f').value;
+    //animal species
+    var especie = document.getElementById("Canine");
+    var especie2 = document.getElementById("Feline");
+    //sex of the animals
+    var sexo = document.getElementById("m");
+    var sexo2 = document.getElementById("f");
+    //dangerous of the 
+    var peligroso = document.getElementById("true");
+    var peligroso2 = document.getElementById("false");
+
+    var microchip = document.getElementById('Microchip').value;
+    var species;
+    if(especie.checked == true)
+      species = especie.value;
+    else if (especie2.checked == true)
+      species = especie2.value;
+
+    var sex;
+    if(sexo.checked == true)
+      sex = sexo.value;
+    else if (sexo2.checked == true)
+      sex = sexo2.value;
+
     var size = document.getElementById('size').value;
-    var potentDangerous = document.getElementById('t').value;
-    var potentDangerous2 = document.getElementById('f').value;
+
+    var potentDangerous;
+    if(peligroso.checked == true)
+      potentDangerous = peligroso.value;
+    else if (peligroso2.checked == true)
+      potentDangerous = peligroso2.value;
+
     var neighborhood = document.getElementById('neighborhood').value;
     var race = document.getElementById('race').value;
     var owner = document.getElementById('owner').value;
     var address = document.getElementById('address').value;
-    /*console.log(escribir); */
 
-    alData.push({"microchip" : microchip, "species" : species || species2, "sex" : sex || sex2, "size" : size, 
-      "potentDangerous" : potentDangerous || potentDangerous2, "neighborhood" : neighborhood, "race" : race, "owner" : owner, "address" : address});
-    printData(); 
+    alData.push({"microchip" : microchip, "species" : species, "sex" : sex, "size" : size, 
+      "potentDangerous" : potentDangerous, "neighborhood" : neighborhood, "race" : race, "owner" : owner,
+       "address" : address});
+     
 
     
  }
