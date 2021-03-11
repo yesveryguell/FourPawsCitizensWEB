e7d6e9d7d99fc7e6ebaed4fa0310bf741678ee1b
Felipe Rojas <jfrojasr@unbosque.edu.co>
2021-03-10 19:32:16 -0500

    error updated, no errors anymore

--- js/load-csv.js
+++ js/load-csv.js
@@ -4,6 +4,7 @@ d3.dsv( ";", "../Data/pets-citizens.csv" )
   .then( function( data ) {
   	alData = data; 
   	printData(); 
+    prueba();
   } )
 
   .catch( function( error ) {
@@ -18,11 +19,21 @@ d3.dsv( ";", "../Data/pets-citizens.csv" )
 
     console.log("robin hijo de la verga");
     var microchip = document.getElementById('Microchip').value; 
-    var species = document.getElementById('cf').value; 
-    var species = document.getElementById('mh').value; 
-    console.log(escribir); 
+    var species = document.getElementById('c').value; 
+    var species2 = document.getElementById('f').value; 
+    var sex = document.getElementById('m').value;
+    var sex2 = document.getElementById('f').value;
+    var size = document.getElementById('size').value;
+    var potentDangerous = document.getElementById('t').value;
+    var potentDangerous2 = document.getElementById('f').value;
+    var neighborhood = document.getElementById('neighborhood').value;
+    var race = document.getElementById('race').value;
+    var owner = document.getElementById('owner').value;
+    var address = document.getElementById('address').value;
+    /*console.log(escribir); */
 
-    allData.push({"microchip" : "1312312", "species" : "1312312", "sex" : "1312312", "size" : "1312312", "potentDangerous" : "1312312", "neighborhood" : "1312312"});
+    alData.push({"microchip" : microchip, "species" : species || species2, "sex" : sex || sex2, "size" : size, 
+      "potentDangerous" : potentDangerous || potentDangerous2, "neighborhood" : neighborhood, "race" : race, "owner" : owner, "address" : address});
     printData(); 
 
     
